010f89440aec8ea5325d3f7982c4e3ceddc029a4
falko.wiese <fwiese.wse@gmail.com>
2017-01-15 02:23:30 +0100

    Added a correct header to all C++ files.

--- ua_server/main.cpp
+++ ua_server/main.cpp
@@ -1,32 +1,26 @@
-/******************************************************************************
-** main.cpp
-**
-** Copyright (c) 2006-2015 Unified Automation GmbH All rights reserved.
-**
-** Software License Agreement ("SLA") Version 2.5
-**
-** Unless explicitly acquired and licensed from Licensor under another
-** license, the contents of this file are subject to the Software License
-** Agreement ("SLA") Version 2.5, or subsequent versions
-** as allowed by the SLA, and You may not copy or use this file in either
-** source code or executable form, except in compliance with the terms and
-** conditions of the SLA.
-**
-** All software distributed under the SLA is provided strictly on an
-** "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED,
-** AND LICENSOR HEREBY DISCLAIMS ALL SUCH WARRANTIES, INCLUDING WITHOUT
-** LIMITATION, ANY WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-** PURPOSE, QUIET ENJOYMENT, OR NON-INFRINGEMENT. See the SLA for specific
-** language governing rights and limitations under the SLA.
-**
-** The complete license agreement can be found here:
-** http://unifiedautomation.com/License/SLA/2.5/
-**
-** Project: C++ OPC-UA Server SDK sample code
-**
-** Description: Main entry for the application
-**
-******************************************************************************/
+/*
+ * This file is part of the UACL (Unified Architecture Convenience
+ * Layer) project.
+ *
+ * Copyright (C) 2017 Falko Wiese (wieSE Software Engineering,
+ *                                 Neubrandenburg)
+ *
+ * Project website ... https://gitlab.com/falko.wiese/uacl_cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <uacl_utils/LoggingHelper.h>
 #include "uacl_server/Server.h"
 #include "uacl_utils/ExceptionHandling.h"
--- uacl_server/InternalNodeManager.h
+++ uacl_server/InternalNodeManager.h
@@ -1,6 +1,25 @@
-//
-// Created by Falko Wiese on 2/9/16.
-//
+/*
+ * This file is part of the UACL (Unified Architecture Convenience
+ * Layer) project.
+ *
+ * Copyright (C) 2017 Falko Wiese (wieSE Software Engineering,
+ *                                 Neubrandenburg)
+ *
+ * Project website ... https://gitlab.com/falko.wiese/uacl_cpp
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef UACL_INTERNALNODEMANAGER_H
 #define UACL_INTERNALNODEMANAGER_H
@@ -25,7 +44,7 @@ namespace uacl_server
         UA_DISABLE_COPY(InternalNodeManager);
 
     public:
-        InternalNodeManager(const QString &name);
+        explicit InternalNodeManager(const QString &name);
 
         ~InternalNodeManager();
 
